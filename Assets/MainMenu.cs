1e3eb91b52c64c8287c265d7141ccf882cc0f910
petrevsky <chrispetrevsky@gmail.com>
2022-08-30 23:09:37 +0200

    First commit to git

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void showRings()
+    {
+        SceneManager.LoadScene("SwipeMenuScene");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void downloadTarget()
+    {
+        Application.OpenURL("https://drive.google.com/file/d/1LRQ6EWUs0Zx32UDGyVBbC-CCi2CS245u/view?usp=sharing");
+    }
+
+    public void help()
+    {
+        SceneManager.LoadScene("HelpScene");
+    }
+}
---
+++ Assets/SwipeRings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwipeRings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void showRing1()
+    {
+        SceneManager.LoadScene("RingScene");
+    }
+
+    public void showRing2()
+    {
+        SceneManager.LoadScene("Ring2scene");
+    }
+
+    public void showRing3()
+    {
+        SceneManager.LoadScene("ring3scene");
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
